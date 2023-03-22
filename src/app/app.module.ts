83b57607b5acaae23337f29fac0ff6bb18e76e19
John Gualteros <gualterosjohn40@gmail.com>
2023-03-21 20:10:56 -0500

    ssr and image optimization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgOptimizedImage } from '@angular/common'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -30,9 +30,10 @@ export function HttpLoaderFactory(http: HttpClient) {
     ProjectsComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HttpClientModule,
+    NgOptimizedImage,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
